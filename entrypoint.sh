f350ea09f57e2c50713dd4a51a3fb39ef7541076
Ivy Feraco <unjust@gmail.com>
2020-03-08 23:31:15 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,9 +30,9 @@ for folder in $FOLDER/*; do
   # clone, delete files in the clone, and copy (new) files over
   # this handles file deletions, additions, and changes seamlessly
   git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$NAME.git $CLONE_DIR &> /dev/null
+  cd $CLONE_DIR
   git fetch origin $BRANCH_NAME
   git checkout -t origin/$BRANCH_NAME
-  cd $CLONE_DIR
   # find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
   cp -r $BASE/$folder/. .
 
